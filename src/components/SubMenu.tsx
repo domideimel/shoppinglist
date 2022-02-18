12debd0e11888fd77ffccb4bd003a3cf4bbffce0
dominikdeimel <info@dominikdeimel.com>
2022-02-18 17:05:31 +0100

    chore: changed menu based on user

--- src/components/SubMenu.tsx
+++ src/components/SubMenu.tsx
@@ -2,8 +2,11 @@ import { FC } from 'react'
 import { Link } from 'react-router-dom'
 import supabase from '../lib/api'
 import { toast } from 'react-toastify'
+import { useAuth } from '../store/auth'
 
 const SubMenu: FC = () => {
+  const user = useAuth(state => state.user)
+
   const handleLogout = async (e: any) => {
     e.preventDefault()
     try {
@@ -13,12 +16,14 @@ const SubMenu: FC = () => {
       toast(error.message, { type: 'error' })
     }
   }
+
   return (
     <>
       <ul className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52 right-0">
-        <li><a onClick={handleLogout} href="#">Logout</a></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Registrieren</Link></li>
+        {user ? <li><a onClick={handleLogout} href="#">Logout</a></li> : <>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register">Registrieren</Link></li>
+        </>}
       </ul>
     </>
   )
